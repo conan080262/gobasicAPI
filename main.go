8944920dadb154ee5f7eaa784d5dc5c8e1d35e3e
nontawat_pm <codenon@hotmail.com>
2021-12-13 20:09:28 +0700

    add learning var

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/conan080262/gobasicAPI/variables"
+	"rsc.io/quote/v3"
+)
+
+func main() {
+	// fmt.Println("Hello World!")
+	fmt.Println(quote.HelloV3())
+	variables.Learn()
+}
