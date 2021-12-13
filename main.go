11b00ecf8efead6afab1ce3d685766db279cb672
nontawat_pm <codenon@hotmail.com>
2021-12-13 20:45:27 +0700

    variable done

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/conan080262/gobasicAPI/variables"
-	"rsc.io/quote/v3"
 )
 
 func main() {
+
 	// fmt.Println("Hello World!")
-	fmt.Println(quote.HelloV3())
+	// fmt.Println(quote.HelloV3())
 	variables.Learn()
 }
--- variables/learn.go
+++ variables/learn.go
@@ -1,7 +1,32 @@
 package variables
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+var fullname string = "Non"
+var email string = "codenon@hotmail.com"
+var c, python bool = true, false
+
+const vat int = 7
 
 func Learn() {
-	fmt.Println("Learn Var")
+	fullname = "Nontawat Pomsanam"
+	fmt.Println(email)
+	fmt.Printf("Myname is %s\n", fullname)
+	fmt.Println(c, python)
+
+	companyName := "SuwanService998"
+	isShow := true
+	age := 10
+
+	fmt.Println(companyName, isShow, age)
+	fmt.Printf("%T\n", isShow)
+
+	f := float64(age)
+	fmt.Printf("%T\n", f)
+
+	s := strconv.Itoa(vat)
+	fmt.Println("int to String Vat is " + s)
 }
